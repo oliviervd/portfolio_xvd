8645344586c258d23c259204e980394812336cd8
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-11-05 15:57:22 +0100

    fucking major update.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Db from "./components/db";
+import portfolioGrid from "./components/portfolioGrid";
+import PortfolioGrid from "./components/portfolioGrid";
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Xavier Van D'huynslager</h1>
+    <div>
+        <div className="grid-3-7">
+            <div className="H_margin-20p">
+                <h1 className="upper">Xavier Van D'huynslager</h1>
+                <p>
+                    Xavier Van D'huynslager is a freelance Director of Photography based in Ghent, Belgium.
+                    -Available for global hire.- www.xaviervandhuynslager.be +32 (0) 496 70 36 47
+                </p>
+            </div>
+            <div>
+            </div>
+        </div>
+        <div className="margin-20p">
+            <div className="line"></div>
+        </div>
+        <div className="margin-20p">
+            <p><Db/></p>
+        </div>
+
+        <div className="grid-3-7">
+            <div>
+            </div>
+            <div>
+                <PortfolioGrid></PortfolioGrid>
+            </div>
+        </div>
+
+
     </div>
   );
 }
