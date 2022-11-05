7c01c2cf330afa51b5803bd65ebc89015ae51af7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-11-05 17:39:43 +0100

    add theater (for homepage)

---
+++ src/components/theater.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Vimeo from "@u-wave/react-vimeo";
+import useWindowDimensions from "./getWindowDimensions";
+
+const Theater = (props) => {
+
+    const { height, width } = useWindowDimensions();
+    const divStyle = {
+        width: width,
+        height: height
+    }
+    return(
+        <div style ={divStyle}>
+            <Vimeo className="theater"
+                   video={props.vimeo_id}
+                   autoplay={true}
+                   width={width}
+                   showTitle={false}
+                   muted={true}
+            />
+        </div>
+    )
+}
+
+export default Theater;
