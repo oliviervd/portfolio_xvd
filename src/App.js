fcf62f95387919e5bafd76900ef18bd467f0ea1e
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-11-05 17:39:57 +0100

    restructure app.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import './App.css';
 import PortfolioGrid from "./components/portfolioGrid";
+import Theater from "./components/theater";
 
 function App() {
 
-  return (
-    <div>
-        <div className="grid-3-7">
-            <div className="H_margin-20p">
-                <h1 className="upper">Xavier Van D'huynslager</h1>
-                <p>
-                    Xavier Van D'huynslager is a freelance Director of Photography based in Ghent, Belgium.
-                    -Available for global hire.- www.xaviervandhuynslager.be +32 (0) 496 70 36 47
-                </p>
-            </div>
+    return (
+        <div>
             <div>
-            </div>
-        </div>
-        <div className="margin-20p">
-            <div className="line"></div>
-        </div>
-        <div className="margin-20p">
-        </div>
+                <div className="grid-3-7">
+                    <div className="H_margin-20p">
+                        <h1 className="upper">Xavier Van D'huynslager</h1>
+                        <p>
+                            Xavier Van D'huynslager is a freelance Director of Photography based in Ghent, Belgium.
+                            -Available for global hire.- www.xaviervandhuynslager.be +32 (0) 496 70 36 47
+                        </p>
+                    </div>
 
-        <div className="grid-3-7">
-            <div>
+                </div>
+
+                <div className="line"></div>
+                <div className="line"></div>
+
+                <Theater vimeo_id="668564452"/>
+
+                <div className="grid-3-7">
+                    <div className="H_margin-20p">
+                        <PortfolioGrid/>
+                    </div>
+                 </div>
+
             </div>
-            <div>
-                <PortfolioGrid></PortfolioGrid>
-            </div>
+
+
+
         </div>
-
-
-    </div>
-  );
+    );
 }
 
 export default App;
