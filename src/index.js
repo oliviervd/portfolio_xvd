9203f48464ad2c0b10382a8fda9a307ae0846bf2
oliviervd <olliviervandhuynslager@gmail.com>
2022-11-06 14:45:50 +0100

    make multipage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {render} from "react-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
-import App from './App';
+import './App.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Home from "./components/pages/home";
+import NarrativeContentHome from "./components/pages/narrativeContentHome";
+import BrandedContentHome from "./components/pages/brandedContentHome";
+import ClientSpace from "./components/pages/clientSpace";
+
+const rootElement = document.getElementById('root');
+render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/narrative" element={<NarrativeContentHome/>} />
+            <Route path="/branded" element={<BrandedContentHome/>} />
+            <Route path="/client-space" element={<ClientSpace/>} />
+        </Routes>
+    </BrowserRouter>,
+    rootElement
 );
