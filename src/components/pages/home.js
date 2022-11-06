d6d27342f5a0b6bb95310cf187642239b86d3ff4
oliviervd <olliviervandhuynslager@gmail.com>
2022-11-06 14:46:40 +0100

    add homepage

---
+++ src/components/pages/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Theater from "../elements/theater";
+import PortfolioGrid from "../elements/portfolioGrid";
+import Menu from "../elements/menu";
+import Header from "../elements/header";
+
+const Home = () => {
+    return(
+        <div>
+            <div>
+                <Header/>
+                <Theater vimeo_id="668564452"/>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Home;
