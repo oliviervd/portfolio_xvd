d36395b63633fdea98fa47c04ac145cb188e89d4
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-11-06 11:58:19 +0100

    add function to fetchKind (narrative / branded)

--- src/components/db_parser.js
+++ src/components/db_parser.js
@@ -2,32 +2,42 @@ import React from "react";
 
 // reusable functions to fetch information from google spreadsheet.
 
-export function fetchDescription(item){
-    if (item.description !== "") {
-        return <p>{item.description}</p>;
+export function fetchType(i) {
+    let type;
+    if (i.type !== "") {
+        type = i.type; // narrative content or branded content
+    } else {
+        type = "unclassified" // if no type classified.
+    }
+    return type;
+}
+
+export function fetchDescription(i) {
+    if (i.description !== "") {
+        return <p>{i.description}</p>;
     };
 }
 
-export function fetchDirectedBy(item){
-    if (item.directed_by !== "") {
-        return <p>directed by: {item.directed_by}</p>
+export function fetchDirectedBy(i) {
+    if (i.directed_by !== "") {
+        return <p>directed by: {i.directed_by}</p>
     };
 }
 
-export function fetchProducedBy(item){
-    if (item.produced_by !== "") {
-        return <p>produced by: {item.produced_by}</p>
+export function fetchProducedBy(i) {
+    if (i.produced_by !== "") {
+        return <p>produced by: {i.produced_by}</p>
     };
 }
 
-export function fetchCinematographer(item){
-    if (item.cinematography !== "") {
-        return <p>cinematography: {item.cinematography}</p>
+export function fetchCinematographer(i) {
+    if (i.cinematography !== "") {
+        return <p>cinematography: {i.cinematography}</p>
     };
 }
 
-export function fetchArtDirection(item){
-    if (item.art_direction !== "") {
-        return <p>art direction: {item.art_direction}</p>
+export function fetchArtDirection(i) {
+    if (i.art_direction !== "") {
+        return <p>art direction: {i.art_direction}</p>
     };
 }
