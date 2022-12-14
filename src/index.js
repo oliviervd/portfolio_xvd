bc3e9b42ff4c074e4bbd5e8bcf3dc2aff7377f3e
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-12-14 07:59:23 +0100

    split ccs (add seperate for gridSystem)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from "react-dom";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './assets/css/App.css'
+import './assets/css/grid.css'
 
 import Home from "./components/pages/home";
 
