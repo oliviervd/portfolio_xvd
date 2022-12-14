bf34ef953fb21f93289f4071b34b95aebe47faad
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-12-14 07:33:48 +0100

    folder redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from "react-dom";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import './App.css'
+import './assets/css/App.css'
 
 import Home from "./components/pages/home";
 
