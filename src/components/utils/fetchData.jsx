4e3067551a374bf5c5e1005fd990b95c5f5a8b3b
oliviervd <oliviervandhuynslager@gmail.com>
2023-11-12 18:05:07 +0100

    add hook for fetching payload data

--- src/components/utils/fetchData.jsx
+++ src/components/utils/fetchData.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import useGoogleSheets from "use-google-sheets";
+import { useQuery } from "@tanstack/react-query";
+
+export function fetchContentPayload() {
+  const { data, isLoading, status } = useQuery({
+    queryKey: ["PROJECTS"],
+    queryFn: () =>
+      fetch("https://p01--admin--jh7ls6pxcdjh.code.run/api/project/", {
+        credentials: "include",
+        method: "GET",
+      }).then((req) => req.json()),
+  });
+  return data;
+}
 
 //todo
 function FetchData() {
-   let data;
-   return data;
+  let data;
+  return data;
 }
 export default FetchData;
+
