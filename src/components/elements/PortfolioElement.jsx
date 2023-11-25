b5e1225b64e7685cbcca19e421e08a89596969e2
oliviervd <oliviervandhuynslager@gmail.com>
2023-11-25 13:48:30 +0100

    connect Payload CMS to frontend app - init

--- src/components/elements/PortfolioElement.jsx
+++ src/components/elements/PortfolioElement.jsx
@@ -10,114 +10,120 @@ const PortfolioElement = (props) => {
     project = _projects["docs"];
   }
 
+  console.log(project);
+
   return (
     <div>
-      {project.map((item) => {
-        console.log(item);
-        let description,
-          title,
-          directed_by,
-          vimeo_id,
-          sort,
-          kind,
-          _img,
-          type,
-          id,
-          year,
-          techSpec;
-        id = item.id; // id of the project (Database).
-        title = item.projectTitle; // title of the project
-        description = item.projectInformation.description[0].children[0].text;
-        directed_by = item.projectInformation.directedBy.firstName;
-        vimeo_id = item.mediaGroup.vimeoURI; // id to fetch vimeo video
-        sort = props.kind; // feature, short, ...
-        kind = item.projectInformation.projectCategory; //branded content or narrative content
-        // add type
-        // add image
-        // add year
-        // add techspecs
-        //tempDB for detailed;
-        let detailDB = [];
-        detailDB.push({
-          vimeo: vimeo_id,
-          title: title,
-          description: description,
-          type: kind,
-          year: year,
-          techSpecs: techSpec,
-        });
+      {project && (
+        <div>
+          {project.map((item) => {
+            let description,
+              title,
+              directed_by,
+              vimeo_id,
+              sort,
+              kind,
+              _img,
+              type,
+              id,
+              year,
+              techSpec;
+            id = item.id; // id of the project (Database).
+            title = item.projectTitle; // title of the project
+            description =
+              item.projectInformation.description[0].children[0].text;
+            directed_by = item.projectInformation.directedBy.firstName;
+            vimeo_id = item.mediaGroup.vimeoURI; // id to fetch vimeo video
+            sort = props.kind; // feature, short, ...
+            kind = item.projectInformation.projectCategory; //branded content or narrative content
+            // add type
+            // add image
+            // add year
+            // add techspecs
+            //tempDB for detailed;
+            let detailDB = [];
+            detailDB.push({
+              vimeo: vimeo_id,
+              title: title,
+              description: description,
+              type: kind,
+              year: year,
+              techSpecs: techSpec,
+            });
 
-        console.log(detailDB);
+            console.log(detailDB);
 
-        function activateDetailViewer() {
-          // function to set state of what will be shown in the detail viewer and which one to open.
-          props.setShowWorkID(id);
-          if (kind === "narrative-content") {
-            props.setDetailNarrativeWindowOpen(true); // set detail pane open
-            // props.setDetailBrandedWindowOpen(false); //todo: check maybe not used
-            props.setHideBranded(true); // hide branded pane
-            props.setHideNarrative(false); // hide narrative pane
-            props.setDetailDB(detailDB); //populate temp db for detail pane
-          } else if (kind === "branded-content") {
-            props.setDetailNarrativeWindowOpen(true);
-            props.setHideNarrative(true);
-            props.setHideBranded(false);
-            props.setDetailDB(detailDB);
-          }
-        }
+            function activateDetailViewer() {
+              // function to set state of what will be shown in the detail viewer and which one to open.
+              props.setShowWorkID(id);
+              if (kind === "narrative-content") {
+                props.setDetailNarrativeWindowOpen(true); // set detail pane open
+                // props.setDetailBrandedWindowOpen(false); //todo: check maybe not used
+                props.setHideBranded(true); // hide branded pane
+                props.setHideNarrative(false); // hide narrative pane
+                props.setDetailDB(detailDB); //populate temp db for detail pane
+              } else if (kind === "branded-content") {
+                props.setDetailNarrativeWindowOpen(true);
+                props.setHideNarrative(true);
+                props.setHideBranded(false);
+                props.setDetailDB(detailDB);
+              }
+            }
 
-        if (kind.trim() === sort.trim()) {
-          // function to set UPPERCASE in bold when tag is p. -- used for credit list.
+            if (kind.trim() === sort.trim()) {
+              // function to set UPPERCASE in bold when tag is p. -- used for credit list.
 
-          return (
-            <div>
-              <div>
-                {props.type && (
-                  <div className={"grid-1-15-4"}>
-                    <p className={"text-rotate upper typeBox"}>{type}</p>
-                    <div onClick={activateDetailViewer}>
-                      <h1 className="accent upper">{title}</h1>
-                    </div>
-                    <div onClick={activateDetailViewer}>
-                      <p className={"link accent"}>read more</p>
-                    </div>
+              return (
+                <div>
+                  <div>
+                    {props.type && (
+                      <div className={"grid-1-15-4"}>
+                        <p className={"text-rotate upper typeBox"}>{type}</p>
+                        <div onClick={activateDetailViewer}>
+                          <h1 className="accent upper">{title}</h1>
+                        </div>
+                        <div onClick={activateDetailViewer}>
+                          <p className={"link accent"}>read more</p>
+                        </div>
+                      </div>
+                    )}
+                    {!props.type && (
+                      <div className={"grid-1-15-4"}>
+                        <div />
+                        <div onClick={activateDetailViewer}>
+                          <h1 className="accent upper">{title}</h1>
+                        </div>
+                        <div onClick={activateDetailViewer}>
+                          <p className={"link accent"}>read more</p>
+                        </div>
+                      </div>
+                    )}
+                    <VideoViewer vimeo_id={vimeo_id} />
+                    <img className="image_fit" alt={""} src={_img} />
+
+                    {/*if narrative content display like this*/}
+
+                    {kind === "narrative content" && (
+                      <div onClick={activateDetailViewer}>
+                        <p className={"creditList"}>{description}</p>
+                      </div>
+                    )}
+
+                    {/*if branded content display like this*/}
+
+                    {kind === "branded content" && (
+                      <div onClick={activateDetailViewer}>
+                        <p>directed by: {directed_by}</p>
+                      </div>
+                    )}
+                    <div className="blackbox"></div>
                   </div>
-                )}
-                {!props.type && (
-                  <div className={"grid-1-15-4"}>
-                    <div />
-                    <div onClick={activateDetailViewer}>
-                      <h1 className="accent upper">{title}</h1>
-                    </div>
-                    <div onClick={activateDetailViewer}>
-                      <p className={"link accent"}>read more</p>
-                    </div>
-                  </div>
-                )}
-                <VideoViewer vimeo_id={vimeo_id} />
-                <img className="image_fit" alt={""} src={_img} />
-
-                {/*if narrative content display like this*/}
-
-                {kind === "narrative content" && (
-                  <div onClick={activateDetailViewer}>
-                    <p className={"creditList"}>{description}</p>
-                  </div>
-                )}
-
-                {/*if branded content display like this*/}
-
-                {kind === "branded content" && (
-                  <div onClick={activateDetailViewer}>
-                    <p>directed by: {directed_by}</p>
-                  </div>
-                )}
-                <div className="blackbox"></div>
-              </div>
-            </div>
-          );
-        }
-      })}
+                </div>
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/elements/PortfolioList.jsx
+++ src/components/elements/PortfolioList.jsx
@@ -1,109 +1,96 @@
 import React from "react";
-import useGoogleSheets from "use-google-sheets";
-import {
-  fetchDescription,
-  fetchID,
-  fetchTitle,
-  fetchType,
-  fetchVimeo,
-  fetchYear,
-  techSpecs,
-} from "../utils/db_parser";
+import { fetchContentPayload } from "../utils/fetchData";
 
 const PortfolioList = (props) => {
-  const { data, loading, error } = useGoogleSheets({
-    apiKey: "AIzaSyCqIBCTia_C6CvAAixtiost-E3hicp7nl4",
-    sheetId: "1Npm-xqwwadyHybkXHUE7imeuh3PjsQn83Hdfaw0epc0",
-    datasheetsOptions: [{ id: "Sheet1" }],
-  });
-
-  if (loading) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
+  let _projects, project;
+  _projects = fetchContentPayload();
+  if (_projects) {
+    project = _projects["docs"];
   }
-
-  if (error) {
-    return (
-      <div>
-        <p>Error!</p>
-      </div>
-    );
-  }
-
-  let _portfolio = [];
-  data.map((x) => {
-    x.data.map((l) => {
-      _portfolio.push(l);
-    });
-  });
 
   return (
     <div>
-      {_portfolio.map((item) => {
-        let _title = fetchTitle(item);
-        let _year = fetchYear(item);
-        let _type = fetchType(item);
-        let _id = fetchID(item);
-        let _kind = item.kind;
-        let _vimeo_id = fetchVimeo(item);
-        let _description = fetchDescription(item);
-        let _techSpec = techSpecs(item);
+      {project && (
+        <div>
+          {project.map((item) => {
+            let description,
+              title,
+              directed_by,
+              vimeo_id,
+              sort,
+              kind,
+              id,
+              year,
+              techSpec;
+            id = item.id; // id of the project (Database).
+            title = item.projectTitle; // title of the project
+            description =
+              item.projectInformation.description[0].children[0].text;
+            directed_by = item.projectInformation.directedBy.firstName;
+            vimeo_id = item.mediaGroup.vimeoURI; // id to fetch vimeo video
+            sort = props.kind; // feature, short, ...
+            kind = item.projectInformation.projectCategory; //branded content or narrative content
+            // add type
+            // add image
+            // add year
+            // add techspecs
+            //tempDB for detailed;
+            let detailDB = [];
+            detailDB.push({
+              vimeo: vimeo_id,
+              title: title,
+              description: description,
+              type: kind,
+              year: year,
+              techSpecs: techSpec,
+            });
 
-        let _detailDB = [];
-        _detailDB.push({
-          vimeo: _vimeo_id,
-          title: _title,
-          description: _description,
-          type: _type,
-          year: _year,
-          techSpecs: _techSpec,
-        });
+            function activateDetailViewer() {
+              props.setShowWorkID(_id);
+              if (kind === "narrative-content") {
+                props.setDetailNarrativeWindowOpen(true);
+                props.setDetailDB(_detailDB);
+                props.setHideBranded(true);
+              }
+              if (kind === "branded-content") {
+                props.setDetailNarrativeWindowOpen(true);
+                props.setDetailDB(_detailDB);
+                props.setHideBranded(true);
+              }
+            }
 
-        function activateDetailViewer() {
-          props.setShowWorkID(_id);
-          if (_kind === "narrative content") {
-            props.setDetailNarrativeWindowOpen(true);
-            props.setDetailDB(_detailDB);
-            props.setHideBranded(true);
-          }
-          if (_kind === "branded content") {
-            props.setDetailNarrativeWindowOpen(true);
-            props.setDetailDB(_detailDB);
-            props.setHideBranded(true);
-          }
-        }
+            let i = 0,
+              ps = document.getElementsByTagName("p");
+            let len;
+            for (len = ps.length; i < len; i++) {
+              let p = ps[i];
+              p.innerHTML = p.innerHTML.replace(
+                /\b([A-Z]{2,})\b/g,
+                "<b>$1</b>",
+              );
+            }
 
-        let i = 0,
-          ps = document.getElementsByTagName("p");
-        let len;
-        for (len = ps.length; i < len; i++) {
-          let p = ps[i];
-          p.innerHTML = p.innerHTML.replace(/\b([A-Z]{2,})\b/g, "<b>$1</b>");
-        }
-
-        return (
-          <div className={"grid-9-1 lineBottom scroller"}>
-            <div className={"grid-3-4-3"}>
-              <p className={"smaller accent"}>{_type}</p>
-              <p className={"bolder"}>{_title}</p>
-              <p className={"smaller accent"}>{_year}</p>
-            </div>
-            <div>
-              <p
-                className={"smaller accent link"}
-                onClick={activateDetailViewer}
-              >
-                read more
-              </p>
-            </div>
-          </div>
-        );
-      })}
+            return (
+              <div className={"grid-9-1 lineBottom scroller"}>
+                <div className={"grid-3-4-3"}>
+                  <p className={"smaller accent"}>{kind}</p>
+                  <p className={"bolder"}>{title}</p>
+                  <p className={"smaller accent"}>{year}</p>
+                </div>
+                <div>
+                  <p
+                    className={"smaller accent link"}
+                    onClick={activateDetailViewer}
+                  >
+                    read more
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
 export default PortfolioList;
-
