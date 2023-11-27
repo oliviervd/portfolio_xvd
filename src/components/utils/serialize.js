358caff64051fba4b36eab14a56ad4be62e35739
oliviervd <oliviervandhuynslager@gmail.com>
2023-11-27 18:20:41 +0100

    add serialize

---
+++ src/components/utils/serialize.js
@@ -0,0 +1,25 @@
+import escapeHtml from "escape-html";
+import { Text } from "slate";
+
+export function serialize(node) {
+  if (Text.isText(node)) {
+    let string = escapeHtml(node.text);
+    if (node.bold) {
+      string = `<strong>${string}</strong>`;
+    }
+    return string;
+  }
+
+  const children = node.children.map((n) => serialize(n)).join("");
+
+  switch (node.type) {
+    case "quote":
+      return `<blockquote><p>${children}</p></blockquote>`;
+    case "paragraph":
+      return `<p>${children}</p>`;
+    case "link":
+      return `<a href="${escapeHtml(node.url)}">${children}</a>`;
+    default:
+      return children;
+  }
+}
